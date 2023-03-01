82e3d308376b5cc9b51577516da0f3d8c471cdf1
bincy879 <102475972+bincy879@users.noreply.github.com>
2023-03-01 11:47:03 +0530

    Created API with database.

--- amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp1": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- amplify-app/src/app/app.module.ts
+++ amplify-app/src/app/app.module.ts
@@ -3,23 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
+/* new form imports */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { NavComponent } from './nav/nav.component';
 import { FormComponent } from './form/form.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
     NavComponent,
-    FormComponent
+    FormComponent,
+    RestaurantsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AmplifyAuthenticatorModule
+    AmplifyAuthenticatorModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
